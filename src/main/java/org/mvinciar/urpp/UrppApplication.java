98504777e6250960a2fe4c0f064d72cd33e75c72
Matthiouz <matt.vincia@gmail.com>
2018-06-02 16:47:20 +0200

    project structure

---
+++ src/main/java/org/mvinciar/urpp/UrppApplication.java
@@ -0,0 +1,12 @@
+package org.mvinciar.urpp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrppApplication.class, args);
+	}
+}
